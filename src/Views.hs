7875dc9c94d41d6e7a8415f36c03394bca3586cb
FabianVegaA <fabianveal@gmail.com>
2022-07-17 21:51:35 -0400

    fix(index/Views): add h1 and change img avatar

--- src/Views.hs
+++ src/Views.hs
@@ -27,10 +27,11 @@ indexView conn = do
         header $ do
           nav $ do
             a ! class_ "nav-link" ! href "/" $ "Dev Sapiens"
-            let avatarLink = "https://cdn-icons.flaticon.com/png/512/3177/premium/3177440.png?token=exp=1657946917~hmac=36516928182eb7ecabaf8ed23e06055c"
-            a $ do img ! src avatarLink ! width "50" ! height "50"
+            a $ do img ! src "img/default_avatar.png" ! width "50" ! height "50"
         main $ do
           img ! src "img/devsapiens.svg" ! class_ "devsapiens"
+          h1 "Welcome to the blog  Dev Sapiens"
+          p "The blog for the new developers of the future."
           postsHtml
 
 getPosts :: Connection -> ActionM Html
@@ -38,12 +39,12 @@ getPosts conn = do
   postData <- liftIO $ getPostData conn
   creators <- liftIO $ mapM (fmap fromJust . getCreatorById conn . postIdCreator) postData
 
-  return $ H.div $ do mapM_ (uncurry postToHtml) $ zip postData creators
+  return $ H.div ! class_ "post" $ do mapM_ (uncurry postToHtml) $ zip postData creators
   where
-    postToHtml (Post id title content idCreator createdAt updatedAt) creator = section ! class_ "post" $ do
+    postToHtml (Post id title content idCreator createdAt updatedAt) creator = section $ do
       h2 $ toHtml title
       H.div ! class_ "author" $ do
-        img ! src "https://robohash.org/ba1e3a7f80ff6bc4e0f438782686f5ec?set=set4&bgset=&size=400x400" ! width "50" ! height "50"
+        img ! src "img/default_avatar.png" ! width "50" ! height "50"
         h3 $ toHtml $ creatorName creator
-      p $ toHtml content
+      p ! class_ "content_post" $ toHtml content
       H.span $ toHtml $ show createdAt
